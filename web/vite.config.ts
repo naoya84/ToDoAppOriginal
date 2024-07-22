3d1f693ac72164bc799256fea19c16d2c0026eda
naoya <naoya0517.mc@gmail.com>
2024-07-23 07:12:37 +0900

    [web/server]アプリ構築からセットアップまで実施

---
+++ web/vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: "src/tests/setupTests.ts"
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  }
+})
